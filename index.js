9a143761adc35b58bd9760ea311017f6774e40d5
chehirbendabous <91904312+chehirbendabous@users.noreply.github.com>
2024-02-01 17:20:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Dark Side🎉","Dark Side🎃","Dark Side🏆"];
 
 
 let currentIndex = 0;
